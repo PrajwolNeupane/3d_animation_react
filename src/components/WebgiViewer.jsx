7ac7c1e7b05e4ff3e61d49a03d2f665125f0edf0
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-08-18 23:31:27 +0545

    Done for Today

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,85 @@
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+} from "react";
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  mobileAndTabletCheck,
+  CanvasSnipperPlugin,
+} from "webgi";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+function WebgiViewer() {
+  const canvaRef = useRef(null);
+
+  const setupViewer = useCallback(async () => {
+    // Initialize the viewer
+    const viewer = new ViewerApp({
+      canvas: canvaRef.current,
+    });
+
+    // Add some plugins
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+    const camera = viewer.scene.activeCamera;
+    const position = camera.position;
+    const target = camera.target;
+
+    await viewer.addPlugin(GBufferPlugin);
+    await viewer.addPlugin(new ProgressivePlugin(32));
+    await viewer.addPlugin(new TonemapPlugin(true));
+    await viewer.addPlugin(GammaCorrectionPlugin);
+    await viewer.addPlugin(SSRPlugin);
+    await viewer.addPlugin(SSAOPlugin);
+    await viewer.addPlugin(BloomPlugin);
+
+    // This must be called once after all plugins are added.
+    viewer.renderer.refreshPipeline();
+
+    // Import and add a GLB file.
+    await manager.addFromPath("scene-black.glb");
+
+    viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+
+    viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+    window.scrollTo(0, 0);
+
+    let needsUpdate = true;
+
+    viewer.addEventListener("preFrame", () => {
+      if (needsUpdate) {
+        camera.positionTargetUpdated(true);
+        needsUpdate = false;
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    setupViewer();
+  }, []);
+
+  return (
+    <div id="webgi-canvas-container">
+      <canvas id="webgi-canvas" ref={canvaRef} />
+    </div>
+  );
+}
+
+export default WebgiViewer;
